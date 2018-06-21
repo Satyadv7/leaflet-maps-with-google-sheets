af54c060316e82958fcf8d48defe8b929b129731
Satyadv7 <40245197+Satyadv7@users.noreply.github.com>
2018-06-20 22:44:17 -0700

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,2 +1,2 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1kEq2d0uGB7M6lfw2-gZNfJ9e7_4T-iOX-dVqnmkBMq0/edit#gid=0
-';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1kEq2d0uGB7M6lfw2-gZNfJ9e7_4T-iOX-dVqnmkBMq0/edit#gid=0';
+
